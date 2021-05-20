61cd6f5bca873c2f2dd140db0c3ded404c97cfdb
hg8116 <2011.harshgupta@gmail.com>
2021-05-20 21:58:04 +0530

    adding whole yelp-camp

---
+++ seeds/index.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database Connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 200; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      author: "6095159799f6b73050dbe0a3",
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora delectus eius ad a, impedit tempore expedita itaque incidunt laudantium asperiores, consectetur vel nostrum molestiae earum neque dolores optio, animi commodi",
+      price,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
+      images: [
+        {
+          url: "https://res.cloudinary.com/dmitsftii/image/upload/v1620925258/YelpCamp/cgbbqhuuenyivvx17hma.jpg",
+          filename: "YelpCamp/cgbbqhuuenyivvx17hma",
+        },
+      ],
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
